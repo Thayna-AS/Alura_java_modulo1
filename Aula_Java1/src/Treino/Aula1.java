dbfecfbd2f9e44c373f7716efd56ef5c3821d136
Thayna-AS <thayna.almeida.silva23@gmail.com>
2022-07-24 18:39:59 -0300

    subindo meus primeiros códigos

---
+++ Aula_Java1/src/Treino/Aula1.java
@@ -0,0 +1,9 @@
+package Treino;
+
+public class Aula1 {
+
+	public static void main(String args[]) {
+	     
+		System.out.println("hello world");
+	}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TesteVariaveis/java/TestaVariaveis1.java
@@ -0,0 +1,31 @@
+package TesteVariaveis.java;
+
+public class TestaVariaveis1 {
+	
+  public static void main(String[] args) {
+	  System.out.println("hello new test");
+      
+      int idade1 = 10, idade2 = 13, resultado;
+      
+      resultado = idade1 + idade2;
+      
+      System.out.println(resultado);
+            
+      System.out.println(idade1);
+      
+      idade1 = 2 * 10 + 3;
+      
+      System.out.println(idade1);
+      
+      idade1 += 10;
+      
+      System.out.println(idade1);
+      
+      
+      
+      
+      
+      
+      
+  }
+} 
